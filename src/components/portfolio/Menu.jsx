b5af9af6dd34792d33f17c0986224dfb3def6162
devanshpandey2003 <devanshpandey8900@gmail.com>
2025-07-15 16:18:18 +0530

    plusfit

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -6,50 +6,51 @@ import tcp from "../../assets/tcp.png";
 import git from "../../assets/git.png";
 
 const Menu = [
-	{
-		id: 1,
-		image: Work1,
-		title: "CrickShot - Sports Performance Analysis",
-		category: ["AI/ML"],
-		url: "https://v0-crickboost.vercel.app/",
-		repositoryUrl: "https://github.com/devanshpandey2003/CrickShot"
-	},
-	{
-		id: 2,
-		image: Work2,
-		title: "Hand Gesture Controlled Mouse",
-		category: ["AI & ML",],
-		repositoryUrl: "https://github.com/devanshpandey2003/Hand_Mouse"
-	},
-	{
-		id: 3,
-		image: Work3,
-		title: "Resume Ranking System",
-		category: ["AI & ML"],
-		url: "https://v0-normal-resume-ranking-website-o0soob.vercel.app/",
-		repositoryUrl: "https://github.com/devanshpandey2003/Resume_Ranking"
-	},
-	{
-		id: 4,
-		image: Work4,
-		title: "BloodLine - Blood Donation Platform",
-		category: ["Full Stack"],
-		repositoryUrl: "https://github.com/devanshpandey2003/BloodLine/tree/bloodline"
-	},
-	{
-		id: 5,
-		image: tcp,
-		title: "Custom Protocol - A Lightweight Communication Protocol",
-		category: ["Backend"],
-		repositoryUrl: "https://github.com/devanshpandey2003/MyProtocol"
-	},
-		{
-		id: 6,
-		image: git,
-		title: "MiniGit - A Version Control System",
-		category: ["Backend"],
-		repositoryUrl: "https://github.com/devanshpandey2003/MiniGit"
-	},
+  {
+    id: 1,
+    image: Work1,
+    title: "CrickShot - Sports Performance Analysis",
+    category: ["AI/ML"],
+    url: "https://v0-crickboost.vercel.app/",
+    repositoryUrl: "https://github.com/devanshpandey2003/CrickShot",
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "Hand Gesture Controlled Mouse",
+    category: ["AI & ML"],
+    repositoryUrl: "https://github.com/devanshpandey2003/Hand_Mouse",
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "Resume Ranking System",
+    category: ["AI & ML"],
+    url: "https://v0-normal-resume-ranking-website-o0soob.vercel.app/",
+    repositoryUrl: "https://github.com/devanshpandey2003/Resume_Ranking",
+  },
+  {
+    id: 4,
+    image: Work4,
+    title: "PlusFitGym",
+    category: ["Next,js"],
+    url: "https://plus-fitt.vercel.app/",
+    repositoryUrl: "https://github.com/devanshpandey2003/CrickShot",
+  },
+  {
+    id: 5,
+    image: tcp,
+    title: "Custom Protocol - A Lightweight Communication Protocol",
+    category: ["Backend"],
+    repositoryUrl: "https://github.com/devanshpandey2003/PlusFitt",
+  },
+  {
+    id: 6,
+    image: git,
+    title: "MiniGit - A Version Control System",
+    category: ["Backend"],
+    repositoryUrl: "https://github.com/devanshpandey2003/MiniGit",
+  },
 ];
 
 export default Menu;
